8b137fb6a79f7233af40cc2649850f64d4cbed3e
nk5jp <keigo.nagata@gmail.com>
2022-07-11 01:07:59 +0900

    Roomの依存関係の追加、およびサンプルEntityの投入

---
+++ app/src/main/java/jp/nk5/saifu/infra/entity/Account.kt
@@ -0,0 +1,13 @@
+package jp.nk5.saifu.infra.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Account(
+    @PrimaryKey
+    val uid: Int,
+    @ColumnInfo(name = "first_name") val firstName: String?,
+    @ColumnInfo(name = "last_name") val lastName: String?
+)
