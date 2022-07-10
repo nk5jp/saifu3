575c425a58fe7965f4c7569823df9624f29a56ee
nk5jp <keigo.nagata@gmail.com>
2022-07-11 02:22:12 +0900

    エラーが出ない程度にDBからのデータ取得を実装

--- app/src/main/java/jp/nk5/saifu/MainActivity.kt
+++ app/src/main/java/jp/nk5/saifu/MainActivity.kt
@@ -2,10 +2,34 @@ package jp.nk5.saifu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
+import jp.nk5.saifu.infra.AppDatabase
+import jp.nk5.saifu.infra.entity.Account
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val db: AppDatabase by lazy {
+        Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "saifu3-db"
+        ).build()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onStart() {
+        super.onStart()
+        GlobalScope.launch(Dispatchers.IO) {
+            val dao = db.accountDao()
+            val lists = dao.getAll()
+            print(lists)
+        }
+    }
+
 }
