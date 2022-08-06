5471bd6331868245d7ce708d6133fc8c3f666125
nk5jp <keigo.nagata@gmail.com>
2022-08-06 12:58:41 +0900

    振替画面を介しての口座解説画面への遷移実装 #11

--- app/src/main/java/jp/nk5/saifu/fragment/MainFragment.kt
+++ app/src/main/java/jp/nk5/saifu/fragment/MainFragment.kt
@@ -15,26 +15,29 @@ class MainFragment : Fragment() {
     private var _binding: FragmentMainBinding? = null
     private val binding get() = _binding!!
 
-    companion object {
-        fun newInstance() = MainFragment()
-    }
-
     private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentMainBinding.inflate(inflater, container, false)
-        binding.button.setOnClickListener {
+        //ボタンクリック時の遷移処理を付加する
+        binding.button1.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_receiptFragment)
+        }
+        binding.button2.setOnClickListener {
             findNavController().navigate(R.id.action_mainFragment_to_transferFragment)
+        }
+        binding.button3.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_searchFragment)
         }
         return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         // TODO: Use the ViewModel
     }
 
